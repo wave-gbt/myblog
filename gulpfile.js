e0847409b309600c9b17424fe8e64cfae4919eee
wave-gbt <goubotao628@gmail.com>
2019-05-12 10:56:48 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,50 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    cssmin = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin');
-//JS压缩
-gulp.task('uglify', function() {
-    return gulp.src('././public/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('././public/js/'));
-});
-//public-fancybox-js压缩
-gulp.task('fancybox:js', function() {
-    return gulp.src('././public/fancybox/jquery.fancybox.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('././public/fancybox/'));
-});
-//public-fancybox-css压缩
-gulp.task('fancybox:css', function() {
-    return gulp.src('././public/fancybox/jquery.fancybox.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('././public/fancybox/'));
-});
-//CSS压缩
-gulp.task('cssmin', function() {
-    return gulp.src('././public/css/style.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('././public/css/'));
-});
-//图片压缩
-gulp.task('images', function() {
-    gulp.src('././public/img/*.*')
-        .pipe(imagemin({
-            progressive: false
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
+
+// 压缩html
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
         }))
-        .pipe(gulp.dest('././public/img/'));
+        .pipe(gulp.dest('./public'))
 });
-gulp.task('build', ['uglify', 'cssmin', 'images', 'fancybox:js', 'fancybox:css']);
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩js
+// // 压缩public目录下的所有js
+//
+// gulp.task('minify-js', function() {
+//     return gulp.src('./public/js/**/*.js')
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./public/js'));
+// });
+// 压缩图片
+gulp.task('minify-images', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+            [imagemin.gifsicle({'optimizationLevel': 3}),
+                imagemin.jpegtran({'progressive': true}),
+                imagemin.optipng({'optimizationLevel': 7}),
+                imagemin.svgo()],
+            {'verbose': true}))
+        .pipe(gulp.dest('./public/images'))
+});
+// 默认任务
+gulp.task('default', [
+    'minify-html','minify-css','minify-js','minify-images'
+]);
